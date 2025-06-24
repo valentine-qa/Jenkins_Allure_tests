f6bc1d00aa83a929bae3915f145d5720e8c5932c
valentine-qa <valentine.borodich13@gmail.com>
2025-06-24 11:52:09 +0300

    add conf

--- conftest.py
+++ conftest.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 
+from utils import attach
+
+
 # from utils import attach
 
 @pytest.fixture(scope='function')
@@ -26,9 +29,9 @@ def setup_browser(request):
     browser = Browser(Config(driver))
     yield browser
 
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
     # attach.add_video(browser)
 
     browser.quit()
