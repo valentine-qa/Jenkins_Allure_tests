677f092c2ef21f004a0de0190d663e9bfe38d325
valentine-qa <valentine.borodich13@gmail.com>
2025-06-24 11:12:38 +0300

    add conf

--- conftest.py
+++ conftest.py
@@ -1,34 +1,34 @@
-# import pytest
-#
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selene import Browser, Config
-#
-# # from utils import attach
-#
-# @pytest.fixture(scope='function',autouse=True)
-# def setup_browser(request):
-#     options = Options()
-#     capabilities = {
-#         "browserName": "chrome",
-#         "browserVersion": "127.0",
-#         "selenoid:options": {
-#             "enableVNC": True,
-#             "enableVideo": True
-#         }
-#     }
-#     options.capabilities.update(capabilities)
-#     driver = webdriver.Remote(
-#         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#
-#     browser = Browser(Config(driver=lambda: driver))
-#     yield browser
-#
-#     # attach.add_screenshot(browser)
-#     # attach.add_logs(browser)
-#     # attach.add_html(browser)
-#     # attach.add_video(browser)
-#
-#     browser.quit()
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+# from utils import attach
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "127.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    # attach.add_screenshot(browser)
+    # attach.add_logs(browser)
+    # attach.add_html(browser)
+    # attach.add_video(browser)
+
+    browser.quit()
